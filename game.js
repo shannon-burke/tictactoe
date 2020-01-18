dfd32d2e71a9791b289e104b39cced9c6dab9a1a
Shannon <shannonburke.art@gmail.com>
2020-01-18 15:47:18 -0600

    Xs and Os insetion works, game reset works

--- game.js
+++ game.js
@@ -1,24 +1,49 @@
-let xTurn;
-let gameWon = false;
+$(document).ready(function () {
 
-console.log(xTurn);
+    let xTurn = true;
+    let gameWon = false;
+    xMark = "X";
+    oMark = "O";
 
-function changeTurn() {
-    if (xTurn === true) {
-        xTurn = false;
-        console.log("Os turn now!")
-    } else if (xTurn === false) {
-        xTurn = true;
-        console.log("Xs turn now!")
-    }
-    console.log(xTurn);
-};
-
-function beginGame() {
-    console.log("begingame working");
-    $("#test-button").click(function () {
-        console.log("beep");
+    $("#reset").click(function () {
+        $(".game-space").removeClass("marked").empty();
+        console.log("reset clicked");
     });
-};
 
-beginGame();
+
+    function changeTurn() {
+
+        if (xTurn === true) {
+            xTurn = false;
+            //console.log(xMark);
+            //console.log(event.target);
+            $target.append(xMark).hide().fadeIn();
+            console.log("Os turn now!")
+        } else if (xTurn === false) {
+            xTurn = true;
+            $target.append(oMark).hide().fadeIn();
+            console.log("Xs turn now!")
+        }
+
+        $target.addClass("marked");
+    };
+
+    function determineValidTurn() {
+        $target = $(event.target);
+        if ($target.hasClass("marked")) {
+            console.log("this spot it already marked");
+        }
+        else {
+            changeTurn();
+        }
+    }
+
+    function beginGame() {
+        //console.log("begingame working");
+        $(".game-space").click(determineValidTurn);
+    };
+
+
+    beginGame();
+
+});
